804d29c653a759915ca999c3b58dd89792924548
ljy <ljy@zhsftech.club>
2018-11-07 17:08:40 +0800

    add test

---
+++ tests/ByteBuffer.cpp
@@ -0,0 +1,66 @@
+#include <gtest/gtest.h>
+#include <mutiplex/ByteBuffer.h>
+
+using namespace muti;
+
+TEST(consstruct, test_bytebuffer)
+{
+
+    ByteBuffer buffer(4);
+    ASSERT_TRUE(buffer.capacity() == 4);
+
+    ASSERT_TRUE(buffer.has_remaining());
+    ASSERT_TRUE(buffer.remaining() == 4);
+
+
+    int a = 7;
+    buffer.put(&a, sizeof(a));
+
+    ASSERT_FALSE(buffer.has_remaining());
+    ASSERT_TRUE(buffer.remaining() == 0);
+
+    int b;
+    buffer.flip();
+    buffer.get(&b, sizeof(b));
+
+    ASSERT_TRUE(a == b);
+
+
+    ASSERT_TRUE(buffer.position() == 4);
+    buffer.rewind();
+    ASSERT_TRUE(buffer.remaining() == 4);
+    ASSERT_TRUE(buffer.position() == 0);
+}
+
+TEST(test_reset, test_bytebuffer)
+{
+    ByteBuffer buffer(10);
+    ASSERT_TRUE(buffer.capacity() == 10);
+
+    int a = 9;
+    buffer.put(&a, sizeof(a));
+
+    buffer.mark();
+
+    ASSERT_TRUE(buffer.remaining() == 6);
+
+    int c;
+    int b = 8;
+    buffer.put(&b, sizeof(b));
+
+    ASSERT_TRUE(buffer.remaining() == 2);
+
+    buffer.reset();
+    ASSERT_TRUE(buffer.remaining() == 6);
+
+    buffer.get(&c, sizeof(c));
+    ASSERT_TRUE(b == c);
+    ASSERT_TRUE(buffer.remaining() == 2);
+}
+
+int main(int argc, char* argv[])
+{
+    testing::InitGoogleTest(&argc, argv);
+    return RUN_ALL_TESTS();
+}
+
---
+++ tests/CircularBuffer.cpp
@@ -0,0 +1,95 @@
+#include <gtest/gtest.h>
+#include <mutiplex/CircularBuffer.h>
+#include <string>
+
+using namespace muti;
+
+
+TEST(test_cirularbuffer, test_cirularbuffer)
+{
+    CircularBuffer buffer(30);
+    uint32_t size = buffer.capacity();
+    ASSERT_TRUE(size == 32);
+
+    char buf_src[100];
+    char buf_out[100];
+
+    for (int j = 0; j < 10000; ++j) {
+
+        for (int i = 0; i < sizeof(buf_src); ++i) {
+            buf_src[i] = static_cast<char>((i * 10 - 9) ^ i - 1);
+        }
+
+        buffer.put(buf_src, 40);
+
+        buffer.put(buf_src + 40, 60);
+
+        buffer.get(buf_out, 60);
+
+        ASSERT_TRUE(memcmp(buf_src, buf_out, 60) == 0);
+        buffer.get(buf_out + 40, 40);
+        ASSERT_TRUE(memcmp(buf_src + 60, buf_out + 40, 40) == 0);
+    }
+}
+
+TEST(resize_0, test_circularbuffer)
+{
+    char buf_src[100];
+    char buf_dest[100];
+    for (int i = 0; i < 100; ++i) {
+        buf_src[i] = static_cast<char>((i * 10 - 9) ^ i - 1);
+    }
+    CircularBuffer buffer(2);
+
+    buffer.put(buf_src, 100);
+    buffer.get(buf_dest, 100);
+
+    buffer.put(buf_src, 100);
+    buffer.get(buf_dest, 100);
+
+    ASSERT_TRUE(memcmp(buf_src, buf_dest, 100) == 0);
+}
+
+TEST(resize_1, test_circularbuffer)
+{
+    char buf_src[128];
+    char buf_dest[128];
+    for (int i = 0; i < 128; ++i) {
+        buf_src[i] = static_cast<char>((i * 10 - 9) ^ i - 1);
+    }
+    CircularBuffer buffer(128);
+
+    buffer.put(buf_src, 128);
+    ASSERT_TRUE(buffer.full());
+    buffer.get(buf_dest, 64);
+    ASSERT_TRUE(!buffer.full());
+    buffer.get(buf_dest + 64, 64);
+    ASSERT_TRUE(memcmp(buf_src, buf_dest, 128) == 0);
+}
+
+TEST(resize_2, test_circularbuffer)
+{
+    char buf_src[26];
+    for (int i = 0; i < 26; ++i) {
+        buf_src[i] = static_cast<char>('a' + i*8 & i);
+    }
+
+    CircularBuffer buffer(1);
+
+    for (int i = 0; i < 100; ++i) {
+        int c = buf_src[i];
+        buffer.put(&c, sizeof(char));
+    }
+
+    char buf_dst[26];
+
+    uint32_t ret = buffer.get(buf_dst, 26);
+    ASSERT_TRUE(ret = 26);
+}
+
+int main(int argc, char* argv[])
+{
+    testing::InitGoogleTest(&argc, argv);
+    return RUN_ALL_TESTS();
+}
+
